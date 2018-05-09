8764eadc4e3db607b6206c64329dbdba75934aa5
Daniel Davis <danis1911@gmail.com>
2018-05-09 16:17:34 -0400

    prepped for js, prepped for goodreads api integration

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -4,5 +4,13 @@ class Api::V1::BooksController < ApiController
     render json: Book.all
   end
 
+  def search
+    topic = params[:query]
+    parser = Parser.new
+    parser.search(topic)
+    render json: { data: parser.data }
+
+  end
+
 
 end
