76495f387e38db8fa4d7ccaed8520b0e09a06d87
Daniel Davis <danis1911@gmail.com>
2018-05-14 19:25:07 -0400

    transferred onClick functionality to new component

--- app/javascript/packs/containers/ThinkersRelateContainer.js
+++ app/javascript/packs/containers/ThinkersRelateContainer.js
@@ -16,49 +16,57 @@ class ThinkersRelateContainer extends Component {
     this.handleSave = this.handleSave.bind(this)
   }
 
-  handleClick(id){
-    // this.setState({selectedArray: id})
-    if(this.state.selectedArray.includes(id)) {
-      let remove = this.state.selectedArray.indexOf(id)
+  handleClick(book){
+    this.handleSave = this.handleSave.bind(this)
+    if(this.state.selectedArray.includes(book.thinker.id)) {
+      let remove = this.state.selectedArray.indexOf(book.thinker.id)
       this.state.selectedArray.splice(remove, 1)
     } else {
-      this.state.selectedArray.push(id)
+      this.state.selectedArray.push(book.thinker.id)
     }
     this.forceUpdate()
   }
 
   handleSave(event){
     this.props.addNewBooks(this.props.selectedArray)
-
   }
 
-
-
-
-
   render(){
-
+    let duplicateCheck = [];
     let books = this.props.books
-    let styleString = "book";
 
     let thinkers = books.map((book) => {
-      let handleClick = () => this.handleClick(thinkers)
+      if(duplicateCheck.includes(book.thinker.id)) {
+      } else {
+        duplicateCheck.push(book.thinker.id)
+
+      let styleString;
+      if(this.state.selectedArray.includes(book.thinker.id)) {
+        styleString = "selectedbook"
+      } else {
+        styleString = "book"
+      }
+
+      let handleClick = () => this.handleClick(book)
       return (
         <ThinkerTile
-          key={book.thinker.id}
-          id={book.thinker.id}
+          key={book.id}
+          id={book.id}
           name={book.thinker.name}
           handleClick={handleClick}
           styleString={styleString}
           />
         )
+
+      }
+
       })
 
     return (
       <div className="rows">
         <div className="columns medium-6">
           <br/>
-          <button className="savebutton centered" onClick={this.handleSave}>Save Books to Shelf</button>
+          <button className="savebutton centered" onClick={this.handleSave}>Save a Student Teacher Relationship</button>
           {thinkers}
         </div>
       </div>
