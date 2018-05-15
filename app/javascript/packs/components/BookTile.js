72981c6d495b9256a3de1b4ce3d964bf837ad709
Daniel Davis <danis1911@gmail.com>
2018-05-15 19:40:51 -0400

    fixed text fit problems

--- app/javascript/packs/components/BookTile.js
+++ app/javascript/packs/components/BookTile.js
@@ -6,7 +6,7 @@ const BookTile = (props) => {
   return (
     <div className="row" onClick={props.handleClick}>
         <div className={props.styleString}>
-          <span className="title">{props.name}</span>
+          <span className="title">{props.name}</span><br/>
             <span className="author"> by {props.thinker}</span>
         </div>
     </div>
