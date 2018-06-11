3348efb96ad751b9ddb2591ebb3fcaefd5e6c811
danielmdavis <danis1911@gmail.com>
2018-06-11 13:54:09 -0400

    shelf created with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :shelf
+
+  after_create :create_shelf
+
+  def create_shelf
+    Shelf.create(user_id: self.id)
+  end
+
 end
